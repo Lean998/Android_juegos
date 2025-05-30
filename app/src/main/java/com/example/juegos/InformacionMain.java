d4733a17a6452a473f2cd928939e940317701642
Lean998 <leandroaguero83@gmail.com>
2025-05-29 23:52:52 -0300

    vista inicio completada y bases de la vista informacion

--- app/src/main/java/com/example/juegos/InformacionMain.java
+++ app/src/main/java/com/example/juegos/InformacionMain.java
@@ -1,7 +1,7 @@
 package com.example.juegos;
 
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,13 +10,11 @@ public class InformacionMain extends BaseActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informacion);
-        View headerView = findViewById(R.id.main_header);
-        TextView tituloHeader = headerView.findViewById(R.id.hd_title);
+
+        TextView tituloHeader = findViewById(R.id.hd_title);
         tituloHeader.setText(R.string.btnInformacion);
+
         configurarLogoInicio();
         configurarBtnVolver();
     }
-
-
-
 }
